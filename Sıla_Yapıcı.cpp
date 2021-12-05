52f4abf357c7fd422d0423fad247a245334982ee
silaypc <70834309+silaypc@users.noreply.github.com>
2021-12-05 17:34:34 +0300

    Update Sıla_Yapıcı.cpp

--- Sıla_Yapıcı.cpp
+++ Sıla_Yapıcı.cpp
@@ -377,7 +377,7 @@ void main(int argc, char *argv[])
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	glutCreateWindow("Sýla Yapýcý Q2");
+	glutCreateWindow("Sila Yapici");
 
 	glutDisplayFunc(display);
 	glutReshapeFunc(onResize);
